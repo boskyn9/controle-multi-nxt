d8011cdb6ca5554f990eeeeb2d455f85f55659bb
boskyn9 <boskyn9@gmail.com>
2013-08-31 21:55:55 +0000

    ajuste nos exemplos

--- NXTProject/src/examples/SimEx4.java
+++ NXTProject/src/examples/SimEx4.java
@@ -29,9 +29,18 @@ public class SimEx4 {
     // ------------------ Environment --------------------------
     static {
         NxtContext.showNavigationBar();
-        NxtContext.useObstacle("sprites/bar0.gif", 250, 200);
-        NxtContext.useObstacle("sprites/bar1.gif", 400, 250);
-        NxtContext.useObstacle("sprites/bar2.gif", 250, 400);
-        NxtContext.useObstacle("sprites/bar3.gif", 100, 250);
+        
+        NxtContext.useObstacle("sprites/bar0.gif", 200, 10);
+        NxtContext.useObstacle("sprites/bar0.gif", 600, 10);
+        
+        NxtContext.useObstacle("sprites/bar1.gif", 790, 200);
+        NxtContext.useObstacle("sprites/bar1.gif", 790, 600);
+        
+        
+        NxtContext.useObstacle("sprites/bar2.gif", 200, 590);
+        NxtContext.useObstacle("sprites/bar2.gif", 600, 590);
+        
+        NxtContext.useObstacle("sprites/bar3.gif", 10, 200);
+        NxtContext.useObstacle("sprites/bar3.gif", 10, 600);
     }
 }
